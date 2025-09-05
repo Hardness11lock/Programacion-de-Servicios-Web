9cc5f7e13f22f1ee20a71daf7f45abd25d851585
Oliver <oliver.chavez0636@alumnos.udg.mx>
2025-09-04 18:09:31 -0600

    feat(endpoint): agregar manejo de errores HTTP (400, 404, 500)

--- Endpoint/server.js
+++ Endpoint/server.js
@@ -2,16 +2,32 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Endpoint raíz
+// Endpoint Basico
 app.get('/', (req, res) => {
   res.send('¡Hola! Este es mi primer servicio en Node.js 🚀');
 });
 
-// Endpoint adicional
+// Endpoint Basico con saludo
 app.get('/saludo', (req, res) => {
   res.json({ mensaje: "Hola desde el endpoint /saludo" });
 });
 
+// 404 - Recurso no encontrado
+app.get('/error404',(req, res) => {
+  res.status(404).json({error: "Recurso no encontrado"});
+});
+
+// 500 - Error Interno del Servidor
+app.get('/error500',(req, res) => {
+  res.status(500).json({error: "Error Interno del Servidor"});
+});
+
+// 400 - Peticion Incorrecta
+app.get('/error400',(req, res) => {
+  res.status(400).json({error: "Peticion Incorrecta"});
+});
+
+// muestra de uso de nuestro servidor
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
